f1f26ded1bae39278f2f108ec1e19c133823d19b
Skrill0 <142359031+Skrill0@users.noreply.github.com>
2023-08-23 06:09:40 +0400

    Delete xkeen directory

--- xkeen/03_delete/01_delete_xray.sh
+++
@@ -1,9 +0,0 @@
-delete_xray() {
-    INSTALL_DIR="$1"
-    
-    # Проверяем, существует ли файл xray
-    if [ -f "$INSTALL_DIR/xray" ]; then
-        # Удаляем файл xray
-        rm "$INSTALL_DIR/xray"
-    fi
-}
--- xkeen/03_delete/08_delete_tmp.sh
+++
@@ -1,6 +0,0 @@
-delete_xkeen_folder() {
-    TMP_DIR_GLOBAL="$1"
-    
-    # Удаляем папку xkeen и ее содержимое рекурсивно
-    rm -r "$TMP_DIR_GLOBAL/xkeen"
-}
