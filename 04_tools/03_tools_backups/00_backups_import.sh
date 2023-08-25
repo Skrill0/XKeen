e8e3abc8c620109be0bca62722ad2c49eb5d94b0
Skrill0 <142359031+Skrill0@users.noreply.github.com>
2023-08-25 04:32:06 +0400

    Delete 04_tools directory

--- 04_tools/03_tools_backups/00_backups_import.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Модуль логирования
-. "$XTOOLS_DIR/03_tools_backups/01_backups_xray.sh"
-. "$XTOOLS_DIR/03_tools_backups/02_backups_xkeen.sh"
-
--- 04_tools/03_tools_backups/01_backups_xray.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-backup_xray() {
-    if [ "$XRAY_INSTALLED" = "installed" ]; then
-        backup_filename="xray_v$XRAY_CURRENT_VERSION"
-        cp "$INSTALL_DIR/xray" "$BACKUPS_DIR/$backup_filename"
-    fi
-}
