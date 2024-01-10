df65989f00a6fce1c15dde56266b7a067ee70600
Skrill <142359031+Skrill0@users.noreply.github.com>
2024-01-10 20:08:13 +0400

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-
-opkg install curl tar
-curl -s -L https://github.com/Skrill0/XKeen/releases/latest/download/xkeen.tar --output xkeen.tar && tar -xvf xkeen.tar -C /opt/sbin --overwrite > /dev/null && rm xkeen.tar
-xkeen -i
