014a1c609a654934d0c42b8ca0099d149bd38e49
Skrill <142359031+Skrill0@users.noreply.github.com>
2024-01-10 19:36:58 +0400

    Add files via upload

---
+++ optimise_server.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [[ $(sysctl net.ipv4.tcp_available_congestion_control) != *'bbr'* ]]; then
+    echo "tcp_bbr" > /etc/modules-load.d/modules.conf
+fi
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+echo "net.core.rmem_max = 67108864" >> /etc/sysctl.conf
+echo "net.core.wmem_max = 67108864" >> /etc/sysctl.conf
+echo "net.core.netdev_max_backlog = 10000" >> /etc/sysctl.conf
+echo "net.core.somaxconn = 4096" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_syncookies = 1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_tw_reuse = 1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_fin_timeout = 30" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_time = 1200" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_probes = 5" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_intvl = 30" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_max_syn_backlog = 8192" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_max_tw_buckets = 5000" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_mem = 25600 51200 102400" >> /etc/sysctl.conf
+echo "net.ipv4.udp_mem = 25600 51200 102400" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_rmem = 4096 87380 67108864" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_wmem = 4096 65536 67108864" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_mtu_probing = 1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_slow_start_after_idle=0" >> /etc/sysctl.conf
